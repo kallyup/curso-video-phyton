3e535874767750200d08962bea50b49d6932bdbc
Débora Macêdo <deboramacedo886@gmail.com>
2023-01-26 22:05:57 -0300

    correções

--- ex006.py
+++ ex006.py
@@ -3,3 +3,4 @@ num = int(input('digite um numero '))
 #triplo = num * 3
 #raiz = num ** (1 / 2)
 print(f' esse é o número {num} seu dobro {num * 2}, seu triplo {num * 3} e sua raiz quadrada {num ** (1 / 2)}')
+# raiz quadrada pow(n,(1/2))>>> formula propria do python
--- ex009.py
+++ ex009.py
@@ -1,4 +1,7 @@
 num = int(input('digite um numero: '))
 
-for n in range(11):
-    print(f'({num} x {n}) = {num * n}')
+#for n in range(11):
+   # print(f'({num} x {n}) = {num * n}')
+
+print("{}x{} = {}".format(num, 1, num*1))
+#repete isso 10 vezes
