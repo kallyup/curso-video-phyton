f735957532d0bead99b1d4548fcdf6796d633355
Débora Macêdo <deboramacedo886@gmail.com>
2023-01-27 21:13:36 -0300

    alguns exercicios

---
+++ ex016.py
@@ -0,0 +1,6 @@
+#import math
+from math import trunc
+num = float(input('digite um numero: '))
+print(trunc(num))
+
+#trunc pega so a parte inteira do numero
---
+++ ex017.py
@@ -0,0 +1,8 @@
+from math import pow
+#import math
+co = float(input('qual o valor do cateto oposto? '))
+ca = float(input('qual o valor do cateto adjacente? '))
+#h = math.pow(ca, 2) + math.pow(co, 2)
+h = pow(ca, 2) + pow(co, 2)
+print(' hipotenusa {} é a soma do quadrado dos catetos oposto {} e adjacente {}'.format(h, co, ca))
+
