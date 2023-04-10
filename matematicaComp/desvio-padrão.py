3c85dce5309e82cadcc53f467e1393f0d779d8d3
Débora Macêdo <deboramacedo886@gmail.com>
2023-04-10 16:48:28 -0300

    desvio padrão e polinomios

--- matematicaComp/desvio-padrão.py
+++ matematicaComp/desvio-padrão.py
@@ -1,7 +1,13 @@
-import numpy as np
+'''import numpy as np
 
 notas = [8, 7, 9, 10, 2, 7, 9, 10, 9, 3, 8, 5]
 
 desvio_padrao = np.std(notas)
 
-print(f"O desvio padrão é de {desvio_padrao:.2f}")
+print(f"O desvio padrão é de {desvio_padrao:.2f}")'''
+
+import numpy as np
+
+valores = np.array([67.8, 78.6, 54.4, 98.6, 99.4, 130.8, 142.6, 161.6, 142.5, 158.4])
+desviop = np.std(valores)
+print(f"O desvio padrão é de {desviop:.2f}")
--- matematicaComp/polinomio.py
+++ matematicaComp/polinomio.py
@@ -1,4 +1,4 @@
-import numpy as np
+'''import numpy as np
 
 # coordenadas dos pontos
 x = [0, 2, 4]
@@ -11,4 +11,10 @@ coeficientes = np.polyfit(x, y, 2)
 p = np.poly1d(coeficientes)
 
 # impressão do polinômio
+print(p)'''
+from scipy.interpolate import *
+#A(0, 6), B(5, 2) e C(8, 15)
+x = [0, 5, 8]
+y = [0, 2, 15]
+p = lagrange(x, y)
 print(p)
