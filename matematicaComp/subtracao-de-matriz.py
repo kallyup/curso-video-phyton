a12d4438274e964d22a8120c36ebbd65612f6cec
Débora Macêdo <deboramacedo886@gmail.com>
2023-04-10 16:30:04 -0300

    multiplicação de vetores

--- matematicaComp/subtracao-de-matriz.py
+++ matematicaComp/subtracao-de-matriz.py
@@ -1,10 +1,18 @@
 import numpy as np
 
-u = np.array([4, 7, 0, -8])
+'''u = np.array([4, 7, 0, -8])
 v = np.array([9, 17, -12, 21])
 u_v = u - v
 uv = u + v
 print(u_v)
-print(uv)
+print(uv)'''
+
+u = np.array([3, 6, -1])
+v = np.array([11, 14, 9])
+print(u*v)
+'''for c in u:
+    for a in v:
+        uv = (a * c)
+        print(uv)'''
 
 
