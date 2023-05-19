37c6864b3bfc51bcdb579fbd46d16498b2742e80
Débora Macêdo <deboramacedo886@gmail.com>
2023-05-19 15:22:38 -0300

    ex 76

--- ex76.py
+++ ex76.py
@@ -1,4 +1,9 @@
 produtos = ('pão', 3.50, 'leite', 7.99, 'queijo', 4.50)
-print("-"*30)
-print("Mercadinho ASD".center(30))
-print("-"*30)
+print("-"*40)
+print("Mercadinho ASD".center(40))# ou :^40
+print("-"*40)
+for pos in range(0, len(produtos)):
+    if pos % 2 == 0:
+        print(f'{produtos[pos]:.<30}', end='')
+    else:
+        print(f'R${produtos[pos]:>7.2f}')
